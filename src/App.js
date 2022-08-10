84024b7f304e3ace4828e5168066af648064b23f
SalhSthai <firstheart00@gmail.com>
2022-08-10 13:30:41 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const hChange = function (event) {
+  console.log( event.target.value); {/* LAB 02 */ }
+};
+const hcChange = function (event) {
+  console.log( event.target.value); {/* LAB 03 */ }
+};
+const confirm = function (event) {
+  let boo = (window.confirm("Leave for https://google.com"));
+   boo? window.location.href("https://google.com"):event.preventDefault() ;
+  console.log(boo);
+}
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={hChange}></input>{/* LAB 01 */}
+
+      <select onChange={hChange}> {/* LAB 02 */}
+        <option>กรุงเทพ</option>
+        <option>กระบี่</option>
+        <option>กาญจนบุรี</option>
+        <option>กาฬสินธุ์</option>
+        <option>กำแพงเพชร</option>
+      </select>
+
+      {/* LAB 03 */}
+      <input onChange={hcChange} type="checkbox" id="phoneBrand" name="iPhone" value="iPhone" defaultChecked></input>
+      <label htmlFor='iPhone'>iPhone</label>
+      <input onChange={hcChange} type="checkbox" id="phoneBrand" name="Samsung" value="Samsung" defaultChecked></input>
+      <label htmlFor='Samsung'>Samsung</label>
+      <input onChange={hcChange} type="checkbox" id="phoneBrand" name="OPPO" value="OPPO" defaultChecked></input>
+      <label htmlFor='OPPO'>OPPO</label>
+      {/* LAB 04 */}
+      <br></br>
+      <a onClick={confirm} href="https://google.com" >Go!!!!</a>
+
     </div>
   );
 }
