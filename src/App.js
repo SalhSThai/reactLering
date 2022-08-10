559c386f7d9678f44ed31702029ebec6b87f2277
SalhSthai <firstheart00@gmail.com>
2022-08-10 09:53:11 +0700

    learned

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import classes from './App.module.css'
+// CSS styleing
+//1.inline
+//2.External Style sheet
+//3. Module Css
+//4. Module in JS
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+// 1.INLINE CSS
+const style = {
+width: "400px",
+height: "300px",
+border: "1px solid red",
+
+}
+
+function App() {  
+  // 1.INLINE CSS
+  return (<div style={style}> 
+     <h3 style={{color:"green",fontSize: "50px"}}>Inline Style</h3>
+     <div className='w-200 h-100 border-green-2'> external css</div>
+     <div className={classes.test}> module css</div>
+     {/* <div className='{classes.test} {classes.test2} '>multiple module css</div> */}
+
+
+  </div>
   );
 }
 
