070051be376fa7280c19bb68532ba75fbd19b01b
SalhSthai <firstheart00@gmail.com>
2022-08-10 15:59:24 +0700

    การreturn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [isLogged,setIsLogged] = useState(false)
+//if isLogged is true : render welcome back , else : go to login page
+//1. store jsx in a variable
+// const message = isLogged? <h1>welcome back </h1>:<h1>Go to Login page</h1>
+//   return (
+//     <div >{message}</div>
+//   );
+
+  //2. && Operator (null && <h1>Test</h1>)
+// return <div>
+//   {isLogged && <h1>Welcome back</h1>}
+//   {!isLogged && <h1>Go to login Page</h1>}
+// </div>
+
+// 3.Ternary operator
+return (<div>{isLogged?<h1>Welcome back</h1>:<h1>Go to Login Page</h1>}</div>)
+
+
 }
 
 export default App;
