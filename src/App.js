75aedccc3cb61ec15c39ea6e3eb1326faa59dddc
SalhSthai <firstheart00@gmail.com>
2022-08-11 11:27:46 +0700

    form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+// React  เราต้องการ submit form ด่วยต้นเองจุง PreventDefault
+// Event ที่อยู่ ใน form  จะมี State เป็นของตัวเอง ต้อง นำ state React ไปใส่ Value ของ Input นั้นๆ
 
 function App() {
+  const [input,setInput] = useState(''); 
+  const [select,setSelect] = useState('NaN')
+  console.log(select)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div><h1>FORM</h1></div>
+
+<select onChange={e=>setSelect(e.target.value)} value={select}>
+  <option value='1'>Bangkok</option>
+  <option value='2'>Nonthaburi</option>
+  <option value='3' >NaN</option>
+</select>
+<input type="file"></input>
+
+      <input 
+      type="text" 
+      value={input}
+      onChange={event=>setInput(event.target.value)}
+      ></input>
+
     </div>
   );
 }
