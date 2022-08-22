a04f25ed796654315c0d59c8bb23dd9a289dd2db
SalhSthai <firstheart00@gmail.com>
2022-08-22 14:07:08 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import {vPost} from './obj.module'
+
+// ให้สร้างตัวแปร Array เก็บข้อมูล Post ที่เป็น Object มี key คือ id, message, วันที่ Post, ชื่อคนโพสต์
+// นำ ArrayPost มา Render ใน list แล้วแสดงบนหน้า UI
 
 function App() {
+  const [friendsArr, setFriendsArr] = useState([]);
+
+  const vFriendsArr = ['a', 'b', 'c', 'd', 'e', 'f'];
+  
+  const putList1 = arr =>arr.map(item =><li>{item}</li>);
+  const putList2 = (arr) => {
+    return arr.map(item => { return <div>
+      <li>{item.id}</li><br/>
+      <li>{item.message}</li><br/>
+      <li>{item.datePost}</li><br/>
+      <li>{item.userName}</li></div>
+    })
+  };
+
+  console.log(putList2(vPost));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display:'flex', flexDirection:'column',justifyContent:'center',width:'100vw'}}>
+      <ul>{putList1(vFriendsArr)} </ul>
+      <ul>{putList2(vPost)}   </ul>
+      <div style={{border:"1px solid black", width:'80vw',height:'100px'}}><h1></h1>
+      <p></p><p></p>
+      </div>
     </div>
   );
 }
---
+++ src/obj.module.js
@@ -0,0 +1,19 @@
+const  vPost = [
+    {
+      id: 1,
+      message: "Happy new year",
+      datePost: "01-01-2021",
+      userName: "John",
+    },
+    {
+      id: 2,
+      message: "Programing is fun",
+      datePost: "30-06-2021",
+      userName: "jack",},
+    {
+      id: 3,
+      message: "oh Amazing !!!",
+      datePost: "23-07-2021",
+      userName: "Sarah",},
+  ]
+  export {vPost};
