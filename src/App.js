cb7d126f51eb163f02735ca9eb53e8a4f4f78dd3
SalhSthai <firstheart00@gmail.com>
2022-09-16 13:23:35 +0700

    redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { countAction } from './redux/store';
 
 function App() {
+  const count = useSelector(state => state.count.count);
+  const dispatch = useDispatch()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={()=>dispatch(countAction.increase())}>+</button>
+      <button onClick={()=>dispatch(countAction.decrease())}>-</button>
+      <button onClick={()=>dispatch(countAction.increase(2))}>Increase by two</button>
     </div>
   );
 }
---
+++ src/redux/store.js
@@ -0,0 +1,35 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit"
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: { count: 0 },
+    reducers: {
+      increase: (state, action) => {
+        console.log(action);
+        state.count += (action.payload ?? 1)
+      },
+      decrease: (state, action) => {
+        state.count -= 1
+      }
+    }
+  })
+  const authSlice = createSlice({
+    name:'auth',
+    initialState:{user:null,isLogged:false},
+    reducers:{
+      login:(state,action) => {},
+      logout:(state,action)=>{}
+    }
+  })
+  
+  const store = configureStore({
+    reducer:{
+      count:counterSlice.reducer,
+      auth:authSlice.reducer
+    }
+  })
+ const countAction = counterSlice.actions;
+ const authAction = authSlice.actions;
+  export {store,countAction,authAction}
+
+  
